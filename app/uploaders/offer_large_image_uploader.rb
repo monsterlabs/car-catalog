4feeff1f1337dcab9d51a0657d32ec24439fe2f3
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-17 13:50:59 -0500

    Models, uploaders, seeds and migrations for the Users and Offers tables using the gems devise, remarkable and carrierwave

---
+++ app/uploaders/offer_large_image_uploader.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+class OfferLargeImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  process :resize_to_fit => [758, 320]
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'ffaker'
+
+Offer.destroy_all
+(1..6).each do |i|
+  @offer = Offer.new
+  @offer.title = Faker::Lorem.sentence
+  @offer.body = Faker::Lorem.sentence
+  @offer.validUntil = Date.today + i.month
+  @offer.enabled = true
+  @offer.url =  Faker::Internet.http_url
+  @offer.image = File.open("#{Rails.root.to_s}/spec/factories/images/bmw_offer_test_#{i}.jpg")
+  @offer.largeImage = File.open("#{Rails.root.to_s}/spec/factories/images/bmw_offer_test_#{i}_large.jpg")
+  unless @offer.valid?
+    puts @offer.errors.full_messages.join(", ")
+  else
+    @offer.save
+  end
+end
+
+User.destroy_all
+@user = User.new(:email => 'admin@bmw.com.mx', :password => 'qw1234..', :password_confirmation => 'qw1234..', :is_admin => true)
+if @user.valid?
+  @user.save
+else
+  puts @user.errors.full_messages.join(', ')
+end
+

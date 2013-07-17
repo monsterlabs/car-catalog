55b89faeca5e7cb420b811637138ce7b4bacf66d
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-17 10:05:14 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source "https://rubygems.org"
+
+gem "rails", "~> 4.0.0"
+gem "pg", "~> 0.15.1"
+gem "slim", "~> 2.0.0"
+gem "rails_admin", "~> 0.5.0"
+gem "carrierwave", "~> 0.9.0"
+gem "devise", "~> 3.0.0"
+gem "mini_magick", "~> 3.6.0"
+
+group :test do
+  gem "rspec-rails", "~> 2.14.0"
+  gem "ffaker", "~> 1.16.2"
+  gem "simplecov", "~> 0.7.1"
+  gem "email_spec", "~> 1.4.0"
+  gem "factory_girl_rails", "~> 4.2.1"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarCatalog::Application.config.secret_key_base = 'b9a3d0c6f9c35cc9f21847f10dfe428cd2567f53ef5df84c5df826ce354e0ad3e03e51cf1d3908757ddc0ca67e369c072b2b7baff9bad8369d68a31d10759ea2'

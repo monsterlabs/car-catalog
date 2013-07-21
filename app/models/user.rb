21eff5aa344d336e073e500c717d602c85959f0a
Alejandro Juarez Robles <juarlex@gmail.com>
2013-07-21 16:19:18 -0500

    Disabling lockable mode for the User Class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable, :lockable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end

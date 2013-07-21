5e00727a3b5a809370438626f722d93b8e7e600e
Alejandro Juarez Robles <juarlex@gmail.com>
2013-07-21 00:09:33 -0500

    First implementation of API::V1::OffersController class compatible with AFIncrementalStore

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 CarCatalog::Application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   devise_for :users
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  namespace :api do
+    namespace :v1 do
+      resources :offers, :only => [:index, :show]
+    end
+  end
 end

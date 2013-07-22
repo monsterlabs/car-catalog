b1ac6cc69fe41853be79d6b11e8509f7e505bebb
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-22 09:59:46 -0500

    ComparedCar model was added

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,5 @@
 class Brand < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_many :series, class_name: "Serie", inverse_of: :brand, dependent: :destroy
+  has_many :compared_cars, class_name: "ComparedCar", inverse_of: :brand, dependent: :destroy
 end

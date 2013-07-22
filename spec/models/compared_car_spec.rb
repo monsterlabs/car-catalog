3ee07356e1f0a56dd94cf0a397544ae73e16bd4e
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-22 10:23:37 -0500

    Comparative model was added

--- spec/models/compared_car_spec.rb
+++ spec/models/compared_car_spec.rb
@@ -4,4 +4,5 @@ describe ComparedCar do
   it { should validate_presence_of(:modelName) }
   it { should validate_presence_of(:year) }
   it { should belong_to(:brand) }
+  it { should have_many(:comparatives) }
 end

4ff30af6ea948261789bc96030d08de81d5847cb
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-22 10:10:30 -0500

    Feature model was added

---
+++ spec/models/feature_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Feature do
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:descr) }
+  it { should belong_to(:specification) }
+end

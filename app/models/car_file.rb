abaace9ec71c32c73e5cbf96956d3471d18ac454
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-24 03:00:39 -0500

    The method import_xls was added to the after_commit callback in the CarFile model

--- app/models/car_file.rb
+++ app/models/car_file.rb
@@ -1,11 +1,21 @@
+require File.join(Rails.root.to_s, 'lib/car_catalog/car_template_importer')
 class CarFile < ActiveRecord::Base
   validates :xls, :image, presence: true
   validates :imported, inclusion: { in: [true, false] }
-  has_one :car, class_name: "Car", inverse_of: :car_file
+  has_one :car, class_name: "Car", inverse_of: :car_file, dependent: :destroy
   mount_uploader :image, ImageUploader
   mount_uploader :xls, DocumentUploader
 
+  after_commit :import_xls, on: [:create, :update]
+
   def to_s
     [serie, line, model, year].join(' ')
+  end
+
+  def import_xls
+    if self.new_record? or !self.imported?
+      @importer = CarCatalog::CarTemplateImporter.new(self)
+      @importer.import_and_save
+    end
   end
 end

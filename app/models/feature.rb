b19272ccebaed567117564e04e0b8e563bf85990
Alejandro Juarez Robles <juarlex@gmail.com>
2013-07-24 01:03:24 -0500

    The validations were updated in the feature model

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,4 @@
 class Feature < ActiveRecord::Base
-  validates :name, :descr, presence: true
   validates :highlighted, inclusion: { in: [true, false] }
   belongs_to :specification, inverse_of: :features
   has_many :compared_features, class_name: "ComparedFeature", inverse_of: :feature

d3c4945d824add93d6b34f54e39d5bef2e342e3a
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-24 17:52:02 -0500

    Enabling root application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 CarCatalog::Application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  root "rails_admin/main#dashboard"
   namespace :api do
     namespace :v1 do
       resources :offers, :only => [:index, :show]

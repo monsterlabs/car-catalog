4aeb58c75fca60c80b7db904985ec6774f8cd7e9
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-24 01:03:24 -0500

    The validations were updated in the feature model

--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,8 +1,6 @@
 require 'spec_helper'
 
 describe Feature do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:descr) }
   it { should belong_to(:specification) }
   it { should have_many(:compared_features) }
 end

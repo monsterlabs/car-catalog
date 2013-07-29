71676182e76b8ec21f746723baba609a31bd3bd8
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-29 11:04:54 -0500

    Avatar based on CarrierWave uploader was add to th User model and minor changes were added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable, :validatable
+  mount_uploader :avatar, AvatarUploader
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,13 +1,14 @@
 # encoding: utf-8
-class ImageUploader < CarrierWave::Uploader::Base
+class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
+
   storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process :resize_to_fit => [728, 364]
+  process :resize_to_fit => [30, 30]
 
   def extension_white_list
     %w(jpg jpeg gif png)

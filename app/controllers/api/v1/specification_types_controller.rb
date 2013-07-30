487abccfa381641c959291a70b36c207b49eb0c8
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-30 09:34:41 -0500

    Minor improvements for specification_types api

--- app/controllers/api/v1/specification_types_controller.rb
+++ app/controllers/api/v1/specification_types_controller.rb
@@ -2,14 +2,14 @@ class Api::V1::SpecificationTypesController < ApplicationController
   def index
     @specification_types = SpecificationType.all
     respond_to do |format|
-      format.json { render json: @specification_types }
+      format.json { render json: @specification_types, :root => "specificationtypes" }
     end
   end
 
   def show
     @specification_type = SpecificationType.find(params[:id])
     respond_to do |format|
-      format.json { render json: @specification_type }
+      format.json { render json: @specification_type , :root => "specificationtype" }
     end
   end
 end

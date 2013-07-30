1d6dc430918040e126f084a2c54fec25213fc78a
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-30 09:34:41 -0500

    Minor improvements for specification_types api

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,21 @@ CarCatalog::Application.routes.draw do
   root "rails_admin/main#dashboard"
   namespace :api do
     namespace :v1 do
-      resources :offers, :only => [:index, :show]
-      resources :brands, :only => [:index, :show]
-      resources :specification_types, :only => [:index, :show]
-      resources :cars, :only => [:index, :show]
+      resources :offers, only: [:index, :show]
+      resources :brands, only: [:index, :show]
+      resources :specification_types, only: [:index, :show]
+      get "/specificationtypes", to: "specification_types#index"
+      get "/specificationtypes/:id", to: "specification_types#show"
+      resources :cars, only: [:index, :show]
       concern :carable do
-        resources :cars, :only => [:index, :show]
+        resources :cars, only: [:index, :show]
       end
-      resources :lines, :only => [:index, :show], concerns: :carable
+      resources :lines, only: [:index, :show], concerns: :carable
 
       concern :lineable do
-        resources :lines, :only => [:index, :show], concerns: :carable
+        resources :lines, only: [:index, :show], concerns: :carable
       end
-      resources :series, :only => [:index, :show], concerns: :lineable
+      resources :series, only: [:index, :show], concerns: :lineable
     end
   end
 end

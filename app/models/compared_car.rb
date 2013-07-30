052224d652d1f8c99ad59505bd9588a48504f9d6
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-30 13:56:31 -0500

    The bulk_delete was enabled for the rails_admin user interface

--- app/models/compared_car.rb
+++ app/models/compared_car.rb
@@ -3,6 +3,7 @@ class ComparedCar < ActiveRecord::Base
   validates :modelName, :year, :presence => true
   belongs_to :brand, inverse_of: :compared_cars
   has_many :comparatives, class_name: "Comparative", foreign_key: "comparedCar_id", inverse_of: :compared_car
+  mount_uploader :image, ImageUploader
 
   def to_s
     [brand.name, modelName, year].join(' ')

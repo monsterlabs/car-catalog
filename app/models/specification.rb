9b6653f64d6ab217ea69e0b4be794a4d6266256a
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-07-30 13:56:31 -0500

    The bulk_delete was enabled for the rails_admin user interface

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -3,4 +3,8 @@ class Specification < ActiveRecord::Base
   belongs_to :specification_type, class_name: "SpecificationType", foreign_key: "specificationType_id", inverse_of: :specifications
   has_many :features, class_name: "Feature", inverse_of: :specification
   has_many :comparatives, class_name: "Comparative", inverse_of: :specification
+
+  def to_s
+    [specification_type.name, car.to_s].join(': ')
+  end
 end
--- app/models/specification_type.rb
+++ app/models/specification_type.rb
@@ -1,5 +1,5 @@
 class SpecificationType < ActiveRecord::Base
   self.table_name = "specificationtypes"
   validates :name, presence: true, uniqueness: true
-  has_many :specifications, class_name: "Specification", inverse_of: :specification_type
+  has_many :specifications, class_name: "Specification", foreign_key: "specificationType_id", inverse_of: :specification_type
 end

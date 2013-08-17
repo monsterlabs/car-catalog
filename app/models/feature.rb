fd87aefea35ee9f91574d06ac0c2f251014e27f6
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-08-17 10:32:14 -0500

    The sequence column and its callbacks were added into the features table

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,17 @@
 class Feature < ActiveRecord::Base
+
   validates :highlighted, inclusion: { in: [true, false] }
+
   belongs_to :specification, inverse_of: :features
   has_many :compared_features, class_name: "ComparedFeature", inverse_of: :feature
+
+  after_validation :increment_sequence, on: [:create]
+
+  default_scope order("features.sequence ASC")
+
+  private
+  def increment_sequence
+    value = ActiveRecord::Migration::execute "SELECT nextval('features_sequence_seq')"
+    self.sequence = value[0]["nextval"].to_i
+  end
 end

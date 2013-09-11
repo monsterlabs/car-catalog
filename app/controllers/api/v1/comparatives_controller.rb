3f9490653c2c322ef9b3ee0cf36f343ab84dedb7
Alejandro Juarez Robles <juarlex@gmail.com>
2013-09-11 09:50:45 -0500

    Relationship was added into the serializers and it was avoided in the index methods of the api::v1 controllers

--- app/controllers/api/v1/comparatives_controller.rb
+++ app/controllers/api/v1/comparatives_controller.rb
@@ -3,14 +3,14 @@ class Api::V1::ComparativesController < ApplicationController
     q = (params.has_key? :specification_id) ? { specification_id: params[:specification_id] } : {}
     @comparatives = Comparative.where(q).all
     respond_to do |format|
-      format.json { render json: @comparatives, :root => "comparatives" }
+      format.json { render json: @comparatives, :root => "comparatives", :except => [:compared_features] }
     end
   end
 
   def show
     @comparative = Comparative.find(params[:id])
     respond_to do |format|
-      format.json { render json: @comparative_type , :root => "comparative" }
+      format.json { render json: @comparative , :root => "comparative" }
     end
   end
 end

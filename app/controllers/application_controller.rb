a218c69cd6ca7863b66b4244560ed0a68dc6927e
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-09-26 11:05:07 -0500

    The authentication based in a simple token was added into the API Controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :restrict_access
+
+  private
+  def is_api_namespace?
+    self.class.name.split("::").first == "Api"
+  end
+
+  def restrict_access
+    if is_api_namespace?
+      api_key = "hKfxHddtln1XPWw1bIwVefodA2p9MROequn/oEG"
+      head :unauthorized unless api_key == request.headers["X-API-TOKEN"]
+    end
+  end
 end

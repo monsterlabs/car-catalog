b4db1bcec2da5236982ec71ccf8d0bba35b1d292
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-10-15 11:05:31 -0500

    Fixing rules for push notifications

--- app/models/concerns/notificable.rb
+++ app/models/concerns/notificable.rb
@@ -2,16 +2,14 @@ module Notificable
   extend ActiveSupport::Concern
 
   included do
-
-    if self == CarFile or self == Offer
+    if self == Car or self == Offer
       after_commit :enqueue_notification, on: [:create, :update, :destroy]
-    else
+    elsif self != CarFile
       after_commit :enqueue_notification, on: [:update, :destroy]
     end
-
   end
 
   def enqueue_notification
-    PushNotification.create(title: ::I18n.t(".notificable.title"), body: ::I18n.t(".notificable.body"), updated_section: self.class.name.downcase)
+      PushNotification.create(title: ::I18n.t(".notificable.title"), body: ::I18n.t(".notificable.body"), updated_section: self.class.name.downcase)
   end
 end
--- app/models/serie.rb
+++ app/models/serie.rb
@@ -4,7 +4,7 @@ class Serie < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :enabled, inclusion: { in: [true, false] }
   belongs_to :brand, inverse_of: :series
-  has_many :lines, class_name: "Line", inverse_of: :serie, dependent: :delete_all
+  has_many :lines, class_name: "Line", inverse_of: :serie, dependent: :destroy
 
   def active_model_serializer
     SerieSerializer

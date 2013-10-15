0e24d4126ec4966899cb0c89da3dfc89a72d4884
Alejandro Juarez Robles <juarlex@gmail.com>
2013-10-15 11:15:59 -0500

    Push notifications for destroy in the serie and line models were updated

--- app/models/serie.rb
+++ app/models/serie.rb
@@ -4,7 +4,16 @@ class Serie < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :enabled, inclusion: { in: [true, false] }
   belongs_to :brand, inverse_of: :series
-  has_many :lines, class_name: "Line", inverse_of: :serie, dependent: :destroy
+  has_many :lines, class_name: "Line", inverse_of: :serie
+
+  before_destroy :destroy_lines
+
+  def destroy_lines
+    lines.each do |l|
+      l.destroy_cars
+    end
+    lines.delete_all
+  end
 
   def active_model_serializer
     SerieSerializer

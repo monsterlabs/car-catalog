1d09a01b3ece124387fbf9c8cfd5144922f4c180
Alejandro Juarez Robles <juarlex@gmail.com>
2013-10-15 11:05:31 -0500

    Fixing rules for push notifications

--- app/models/line.rb
+++ app/models/line.rb
@@ -6,7 +6,13 @@ class Line < ActiveRecord::Base
   validates :enabled, inclusion: { in: [true, false] }
 
   belongs_to :serie, class_name: "Serie", inverse_of: :lines
-  has_many :cars, class_name: "Car", foreign_key: "line_id", inverse_of: :line, dependent: :delete_all
+  has_many :cars, class_name: "Car", foreign_key: "line_id", inverse_of: :line
+
+  before_destroy :destroy_cars
+  
+  def destroy_cars
+    Car.destroy_all "line_id = #{self.id}"
+  end
 
   def active_model_serializer
     LineSerializer

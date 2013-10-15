d2675f76e3aad485627221ecdb4c5b279b80f50f
Alejandro Juarez Robles <juarlex@gmail.com>
2013-10-14 23:32:19 -0500

    Replacing the destroy method with the delete method in the models and  replacing the update_attributes method with the update_columns method in the CarTemplateImporter class to avoid duplicated push notifications

--- app/models/line.rb
+++ app/models/line.rb
@@ -3,10 +3,10 @@ class Line < ActiveRecord::Base
   include Notificable
 
   validates :name, :presence => true
-  validates :enabled, inclusion: { in: [true, false] }, :presence => true
+  validates :enabled, inclusion: { in: [true, false] }
 
   belongs_to :serie, class_name: "Serie", inverse_of: :lines
-  has_many :cars, class_name: "Car", foreign_key: "line_id", inverse_of: :line, dependent: :destroy
+  has_many :cars, class_name: "Car", foreign_key: "line_id", inverse_of: :line, dependent: :delete_all
 
   def active_model_serializer
     LineSerializer
--- app/models/specification_type.rb
+++ app/models/specification_type.rb
@@ -3,5 +3,5 @@ class SpecificationType < ActiveRecord::Base
 
   self.table_name = "specificationtypes"
   validates :name, presence: true, uniqueness: true
-  has_many :specifications, class_name: "Specification", foreign_key: "specificationType_id", inverse_of: :specification_type
+  has_many :specifications, class_name: "Specification", foreign_key: "specificationType_id", inverse_of: :specification_type, dependent: :delete_all
 end

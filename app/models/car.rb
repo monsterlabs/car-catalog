1e6983f007a5db22c6e3e455dd6a10a65448ebda
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-10-15 11:05:31 -0500

    Fixing rules for push notifications

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,12 +6,35 @@ class Car < ActiveRecord::Base
 
   belongs_to :line, class_name: "Line", foreign_key: "line_id", inverse_of: :cars
   belongs_to :car_file, class_name: "CarFile", inverse_of: :car
-  has_many :specifications, class_name: "Specification", inverse_of: :car, dependent: :delete_all
+  has_many :specifications, class_name: "Specification", inverse_of: :car
+
+  before_destroy :destroy_specifications
+  
+  def destroy_specifications
+    # Don't use the destroy method to avoid duplicated Push Notification
+    specifications.each do |s|
+      s.features.delete_all
+      s.comparatives.each do |c|
+        c.compared_features.delete_all
+      end
+      s.comparatives.delete_all
+    end
+    specifications.delete_all
+  end
+
 
   mount_uploader :image, ImageUploader
 
   def to_s
-    [line.serie.name, line.name, modelName, year].join(' - ')
+    [serie_name, line_name, modelName, year].compact.join(' - ')
+  end
+
+  def line_name
+    line.name unless line.nil?
+  end
+
+  def serie_name
+    line.serie.name if !line.nil? and !line.serie.nil?
   end
 
   def active_model_serializer
--- app/models/specification.rb
+++ app/models/specification.rb
@@ -1,8 +1,9 @@
 class Specification < ActiveRecord::Base
   belongs_to :car, class_name: "Car", inverse_of: :specifications
   belongs_to :specification_type, class_name: "SpecificationType", foreign_key: "specificationType_id", inverse_of: :specifications
-  has_many :features, class_name: "Feature", inverse_of: :specification, dependent: :delete_all
-  has_many :comparatives, class_name: "Comparative", inverse_of: :specification, dependent: :delete_all
+  has_many :features, class_name: "Feature", inverse_of: :specification
+  has_many :comparatives, class_name: "Comparative", inverse_of: :specification
+
 
   def to_s
     [specification_type.name, car.to_s].join(': ')

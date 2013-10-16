180e33ed6ca44bc452e4b3d48c5f49e31a5398e9
Alejandro Juarez <karmatr0n.dixit@gmail.com>
2013-10-16 10:44:15 -0500

    The destroy_cars was rewritten and renamed to delete_cars in the line model class

--- app/models/line.rb
+++ app/models/line.rb
@@ -8,13 +8,11 @@ class Line < ActiveRecord::Base
   belongs_to :serie, class_name: "Serie", inverse_of: :lines
   has_many :cars, class_name: "Car", foreign_key: "line_id", inverse_of: :line
 
-  before_destroy :destroy_cars
-  
-  def destroy_cars
-    cars.each do |c|
-      c.destroy_specifications
-    end
-    cars.delete_all
+  before_destroy :delete_cars
+
+  def delete_cars
+    cars.collect(&:delete_specifications)
+    Car.delete_all(line_id: self.id)
   end
 
   def active_model_serializer
